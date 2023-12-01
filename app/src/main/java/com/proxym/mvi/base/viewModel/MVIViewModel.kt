44d3f9c7983ad023d919a0b708cd2c683daae89b
Slama.Taieb <slama.taieb.contact@gmail.com>
2023-12-01 15:25:55 +0100

    insure running update state in viewModelScope

--- app/src/main/java/com/proxym/mvi/base/viewModel/MVIViewModel.kt
+++ app/src/main/java/com/proxym/mvi/base/viewModel/MVIViewModel.kt
@@ -3,6 +3,11 @@ package com.proxym.mvi.base.viewModel
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
 
 
 /**
@@ -16,15 +21,28 @@ import androidx.lifecycle.ViewModel
 
 abstract class MVIViewModel<STATE, EVENT> : ViewModel() {
 
+    private val stateChannel: Channel<STATE> = Channel()
+
     open val viewStates: MutableState<STATE?> = mutableStateOf(null)
+
+    init {
+        stateChannel.receiveAsFlow()
+            .onEach(::updateState)
+            .launchIn(viewModelScope)
+    }
 
     abstract suspend fun initStates()
 
     abstract fun handleEvents(viewEvent: EVENT)
 
     fun moveToState(state: STATE) {
+        stateChannel.trySend(state)
+    }
+
+    private fun updateState(state: STATE) {
         viewStates.value = null
         viewStates.value = state
     }
+
 
 }
