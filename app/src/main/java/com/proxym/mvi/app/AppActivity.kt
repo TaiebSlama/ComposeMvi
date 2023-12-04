c74c4d95dd89317a44bbff656357f092eb1f3262
Slama.Taieb <slama.taieb.contact@gmail.com>
2023-12-04 22:33:11 +0100

    insure running update state in viewModelScope

--- app/src/main/java/com/proxym/mvi/app/AppActivity.kt
+++ app/src/main/java/com/proxym/mvi/app/AppActivity.kt
@@ -1,15 +1,17 @@
-package com.proxym.mvi.features.authentication
+package com.proxym.mvi.app
 
 import androidx.compose.runtime.Composable
 import com.proxym.mvi.base.activity.BaseActivity
+import com.proxym.mvi.features.map.MapScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AuthenticationActivity : BaseActivity() {
+class AppActivity : BaseActivity() {
 
     @Composable
     override fun ActivityLayout() {
-        AuthenticationScreen()
+        //  AuthenticationScreen()
+        MapScreen()
     }
 
 }
---
+++ app/src/main/java/com/proxym/mvi/features/map/MapScreen.kt
@@ -0,0 +1,41 @@
+package com.proxym.mvi.features.map
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+/**
+ * Created by taieb.slama@zeta-box.com on 12/4/2023 .
+ * Copyright (c) 2023 ZETA-BOX. All rights reserved.
+ */
+
+@Composable
+fun MapScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize(), content = { padding ->
+        val singapore = LatLng(1.35, 103.87)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = singapore),
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
+    })
+
+}
